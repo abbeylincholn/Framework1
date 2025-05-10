8f0e9c5ff3efbf563e7202bb241ffd3a0d8e61eb
Abbey <abbeylincholn4real@gmail.com>
2025-05-10 13:09:22 +0100

    Parameterization with testng Data provider. update

--- src/test/java/abbeyLtd/TestPage/SubmitOrderTest.java
+++ src/test/java/abbeyLtd/TestPage/SubmitOrderTest.java
@@ -8,6 +8,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 
 public class SubmitOrderTest extends BaseTest {
@@ -20,13 +21,13 @@ public class SubmitOrderTest extends BaseTest {
        // String productName = "ZARA COAT 3";
 
     @Test(dataProvider = "getData", groups = {"Purchase"})
-    public void submitOrderTest(String userName, String password, String productName) throws InterruptedException, IOException {
-        ProductCatalogue productCatalogue = landingPage.loginApplication(userName, password);
+    public void submitOrderTest(HashMap<String, String> input) throws InterruptedException, IOException {
+        ProductCatalogue productCatalogue = landingPage.loginApplication(input.get("username"), input.get("password"));
         List<WebElement> productsList = productCatalogue.getProductList();
-        productCatalogue.addProductToCart(productName);
+        productCatalogue.addProductToCart(input.get("productName"));
         CartPage cartPage = productCatalogue.goToCardPage();
 
-        Boolean match = cartPage.verifyProductDisplay(productName);
+        Boolean match = cartPage.verifyProductDisplay(input.get("productName"));
         Assert.assertTrue(match);
         CheckoutPage checkoutPage = cartPage.goToCheckoutPage();
         checkoutPage.selectCountry("Nigeria");
@@ -38,17 +39,33 @@ public class SubmitOrderTest extends BaseTest {
     // Dependency attribute test strategy
     //To verify Zara Coat 3 is displaying in orders page.
     @Test (dependsOnMethods = "submitOrderTest", dataProvider = "getData")
-    public void orderHistoryTest(String userName, String password, String productName)  {
-        ProductCatalogue productCatalogue = landingPage.loginApplication(userName, password);
+    public void orderHistoryTest(HashMap<String, String> input)  {
+        ProductCatalogue productCatalogue = landingPage.loginApplication(input.get("username"), input.get("password"));
         OrderPage orderPage = productCatalogue.goToOrderPage();
-        boolean match = orderPage.verifyOrderDisplay(productName);
+        boolean match = orderPage.verifyOrderDisplay(input.get("productName"));
         Assert.assertTrue(match);
     }
 
     @DataProvider
     public Object[][] getData() {
-        return new Object[][] {{"abbeylincoln@gmail.com", "Abbey100!", "ZARA COAT 3"}, {"shetty@gmail.com", "Iamking@000", "ADIDAS ORIGINAL"}};
+        HashMap<String, String> map = new HashMap<>();
+        map.put("username", "abbeylincoln@gmail.com");
+        map.put("password", "Abbey100!");
+        map.put("productName", "ZARA COAT 3");
+
+        HashMap<String, String> map1 = new HashMap<>();
+        map1.put("username", "shetty@gmail.com");
+        map1.put("password", "Iamking@000");
+        map1.put("productName", "ZARA COAT 3");
+        return new Object[][] {{map}, {map1}};
     }
+
+    /*@DataProvider
+	  public Object[][] getData()
+	  {
+	    return new Object[][]  {{"abbeylincoln@gmail.com" ,"Abbey100!","ZARA COAT 3"}, {"shetty@gmail.com","Iamking@000","ADIDAS ORIGINAL" } };
+
+	  }*/
 
 
 
